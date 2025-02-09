2db36938b7dea171ebc5f26b16b54fb0f2f6ac72
Rishiraj Dutta <141042865+rishiorg@users.noreply.github.com>
2025-02-09 16:10:47 +0530

    Time: 17 ms (21.42%) | Memory: 13.8 MB (29.60%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -15,23 +15,26 @@ class Solution(object):
         #     count = max(count,count1)
         #     if count>len(nums)//2:
         #         return i
-        # counts = Counter(nums)
-        # for key, value in counts.items():
-        #     if value>(len(nums)//2):
-        #         return key
-        # return -1
-        count = 0
-        for i in range(len(nums)): 
-            if count == 0:
-                el = nums[i]
-                count = 1
-            elif nums[i] == el:
-                count+=1
-            else:
-                count-=1
-        count1 = 0
-        for i in range(len(nums)):
-            if nums[i] == el:
-                count1+=1
-        if count1>(len(nums)//2):
-            return el
+        counts = Counter(nums)
+        for key, value in counts.items():
+            if value>(len(nums)//2):
+                return key
+        return -1
+
+
+
+        # count = 0
+        # for i in range(len(nums)): 
+        #     if count == 0:
+        #         el = nums[i]
+        #         count = 1
+        #     elif nums[i] == el:
+        #         count+=1
+        #     else:
+        #         count-=1
+        # count1 = 0
+        # for i in range(len(nums)):
+        #     if nums[i] == el:
+        #         count1+=1
+        # if count1>(len(nums)//2):
+        #     return el
