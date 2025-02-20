07eb3ad1ba588282fbedb9f84d49d96d654f4b75
Rishiraj Dutta <141042865+rishiorg@users.noreply.github.com>
2025-02-20 13:47:15 +0530

    Time: 81 ms (27.29%) | Memory: 27.5 MB (15.87%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -24,24 +24,39 @@ class Solution(object):
 
 
     # aprroach 2
-        nums.sort()
+        # nums.sort()
         n = len(nums)
         if  n == 0:
             return 0
+        # longest = 1
+        # count = 0
+        # last_smaller = float('-inf')
+
+        # for i in range(n):
+        #     if nums[i]-1 == last_smaller:
+        #         count+=1
+        #         last_smaller = nums[i]
+        #     elif nums[i] == last_smaller:
+        #         continue
+        #     elif nums[i] != last_smaller:
+        #         count = 1
+        #         last_smaller = nums[i]
+        #     longest = max(longest, count)
+        # return longest
+        st = set()
         longest = 1
         count = 0
-        last_smaller = float('-inf')
-
         for i in range(n):
-            if nums[i]-1 == last_smaller:
-                count+=1
-                last_smaller = nums[i]
-            elif nums[i] == last_smaller:
-                continue
-            elif nums[i] != last_smaller:
+            st.add(nums[i])
+        for it in st:
+            if it-1 not in st:
                 count = 1
-                last_smaller = nums[i]
-            longest = max(longest, count)
+                x = it
+                while x+1 in st:
+                    x+=1
+                    count+=1 
+                longest= max(longest,count)
         return longest
+        
 
         
