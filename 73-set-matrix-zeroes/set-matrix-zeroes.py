efe2888a420e5e5927f7df992df06a2ef5ba5fc7
Rishiraj Dutta <141042865+rishiorg@users.noreply.github.com>
2025-02-27 22:22:51 +0530

    Time: 7 ms (48.95%) | Memory: 13 MB (74.20%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -30,18 +30,43 @@ class Solution(object):
     #     for i in range(n):
     #         if matrix[i][j] != 0:
     #             matrix[i][j]=-1
-        row = [0]*n
-        col = [0]*m
+        # row = [0]*n
+        # col = [0]*m
+        # for i in range(n):
+        #     for j in range(m):
+        #         if matrix[i][j] == 0:
+        #             row[i] = 1
+        #             col[j] = 1
+        
+        # for i in range(n):
+        #     for j in range(m):
+        #         if row[i] or col[j]:
+        #             matrix[i][j] = 0
+
+        # new row matrix[..][0]
+        # new col matrix[0][..]
+        col0 = 1
         for i in range(n):
             for j in range(m):
                 if matrix[i][j] == 0:
-                    row[i] = 1
-                    col[j] = 1
-        
-        for i in range(n):
+                    # mark ith row
+                    matrix[i][0] = 0
+                    # mark jth col
+                    if j != 0:
+                        matrix[0][j] = 0
+                    else:
+                        col0 = 0
+        for i in range(1,n):
+            for j in range(1,m):
+                if matrix[i][j] != 0:
+                    if matrix[i][0] == 0 or matrix[0][j] == 0:
+                        matrix[i][j] = 0
+        if matrix[0][0] == 0:
             for j in range(m):
-                if row[i] or col[j]:
-                    matrix[i][j] = 0
+                matrix[0][j] = 0
+        if col0 == 0:
+            for i in range(n):
+                matrix[i][0] = 0
 
 
                     
