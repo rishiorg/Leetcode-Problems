9f53e399cbbe7fd45c0ca524bdfc2dfbb8bbd088
Rishiraj Dutta <141042865+rishiorg@users.noreply.github.com>
2025-02-28 15:14:02 +0530

    Time: 9 ms (30.74%) | Memory: 14.3 MB (8.77%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.py
+++ 229-majority-element-ii/majority-element-ii.py
@@ -19,13 +19,45 @@ class Solution(object):
         # return ls
          
         #  hashmap approach
-        hashmap = defaultdict(int) 
+        # hashmap = defaultdict(int) 
+        # ls = []
+        # mini = n//3 +1
+        # for num in nums:
+        #     hashmap[num] += 1
+        #     if hashmap[num] >= mini and num not in ls:
+        #         ls.append(num)
+        # return ls
+
+        # optimal
+        count1,count2 = 0,0
+        ele1,ele2 = float('-inf'), float('-inf')
+        for i in range(n):
+            if count1 == 0 and nums[i] != ele2:
+                count1 = 1
+                ele1 = nums[i]
+            elif count2 == 0 and nums[i] != ele1:
+                count2 = 1
+                ele2 = nums[i]
+            elif nums[i] == ele1:
+                count1+=1
+            elif nums[i] == ele2:
+                count2+=1
+            else:
+                count1-=1
+                count2-=1
         ls = []
+        count1,count2 = 0,0
         mini = n//3 +1
-        for num in nums:
-            hashmap[num] += 1
-            if hashmap[num] >= mini and num not in ls:
-                ls.append(num)
+        for i in nums:
+            if ele1 == i:
+                count1+=1
+            elif ele2 == i:
+                count2+=1
+        if count1 >= mini:
+            ls.append(ele1)
+        if count2 >= mini:
+            ls.append(ele2)
+        ls.sort()
         return ls
 
         
