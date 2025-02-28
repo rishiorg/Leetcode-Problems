94d518a14ec7cac0fac90861ca8f9a263936ea61
Rishiraj Dutta <141042865+rishiorg@users.noreply.github.com>
2025-02-28 14:18:33 +0530

    Time: 3 ms (89.61%) | Memory: 13.1 MB (57.19%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.py
+++ 229-majority-element-ii/majority-element-ii.py
@@ -4,17 +4,28 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        ls = []
+        # ls = []
         n = len(nums)
-        for i in range(n):
-            if len(ls) == 0 or ls[0] != nums[i]:
-                cnt = 0
-                for j in range(n):
-                    if nums[i]==nums[j]:
-                        cnt+=1
-                if cnt > n//3:
-                    ls.append(nums[i])
-            if len(ls) == 2:
-                break
+        # for i in range(n):
+        #     if len(ls) == 0 or ls[0] != nums[i]:
+        #         cnt = 0
+        #         for j in range(n):
+        #             if nums[i]==nums[j]:
+        #                 cnt+=1
+        #         if cnt > n//3:
+        #             ls.append(nums[i])
+        #     if len(ls) == 2:
+        #         break
+        # return ls
+         
+        #  hashmap approach
+        hashmap = defaultdict(int) 
+        ls = []
+        mini = n//3 +1
+        for num in nums:
+            hashmap[num] += 1
+            if hashmap[num] >= mini and num not in ls:
+                ls.append(num)
         return ls
+
         
